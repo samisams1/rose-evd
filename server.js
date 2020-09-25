66fe6043ac7453385fc6176ff250449c21ff4089
Ezedin <ezedin.fedlu@gmail.com>
2020-09-25 16:21:00 +0300

    fix

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.listen(3000, () => {
 });
 
 */
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Express app listening on port 3000');
 });
